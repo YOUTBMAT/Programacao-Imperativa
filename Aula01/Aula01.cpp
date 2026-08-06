c07615dfc89ed8206247a72128e14872a7b36074
Mateus Weiss <mateuswmedeiros@gmail.com>
2026-08-06 17:10:58 -0300

    Correção

--- Aula01/Aula01.cpp
+++ Aula01/Aula01.cpp
@@ -1,17 +1,11 @@
 //
 // Created by mateu on 06/08/2026.
 //
-#include <iostream>
+#include <stdio.h>
 
 int main() {
-    std::cout << "=== Teste de C++ ===" << std::endl;
-    std::cout << "Ambiente configurado com sucesso!" << std::endl;
+    
 
-    int numero;
-    std::cout << "Digite um numero inteiro para testar a entrada de dados: ";
-    std::cin >> numero;
 
-    std::cout << "Voce digitou o numero: " << numero << std::endl;
 
-    return 0;
 }
