7fd3377f1d828d24f7ad7e88b0544e7d887ea045
Mateus Weiss Medeiros <mateuswmedeiros@gmail.com>
2026-08-06 11:58:23 -0300

    Arquivo de teste

---
+++ Aula01.cpp
@@ -0,0 +1,17 @@
+//
+// Created by mateu on 06/08/2026.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "=== Teste de C++ ===" << std::endl;
+    std::cout << "Ambiente configurado com sucesso!" << std::endl;
+
+    int numero;
+    std::cout << "Digite um numero inteiro para testar a entrada de dados: ";
+    std::cin >> numero;
+
+    std::cout << "Voce digitou o numero: " << numero << std::endl;
+
+    return 0;
+}
